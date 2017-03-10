a0bfb2859dec4e42578c5c0cfe56f5b6a3e79710
glisterghost <glisterghost@gmail.com>
2017-03-10 12:41:22 +0000

    tests completed

---
+++ TestPda.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class TestPda {
+
+  Pda pda1;
+
+@Before
+public void before(){
+  pda1 = new Pda();
+}
+
+
+@Test
+public void testVal1(){
+  assertEquals(false, pda1.func1(2));
+  assertEquals(true, pda1.func1(1));
+}
+
+@Test 
+public void testMaxA(){
+  assertEquals(8, pda1.max(8, 4));
+}
+
+@Test
+public void testLooper(){
+assertEquals(11, pda1.looper()); 
+}
+
+@Test
+public void testCheckLoop(){
+assertEquals("looper passed", pda1.checkLoop(10));
+}
+
+}
